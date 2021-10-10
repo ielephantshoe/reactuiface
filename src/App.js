9cfc2fb13a565b95a5785409b46d78130117a434
butterflydesign <hey@butterflydesign.app>
2021-10-10 16:13:38 -0400

    added some more components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 import './App.css';
-import { BiArchive} from "react-icons/bi"
+import { BiArchive,BiTrash} from "react-icons/bi"
 import Search from './components/Search';
 import AddApponitment from './components/AddApponitment';
+
+import appointmentList from "./data.json";
+
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
@@ -12,8 +15,28 @@ function App() {
      
      
         </h1>
-       <Search />
        <AddApponitment/>
+       <Search />
+<ul>
+{
+  appointmentList.map(appointment =>(
+   <li className="px-3 py-3 flex items-start">
+  <button type="button"
+    className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+    <BiTrash /></button>
+  <div className="flex-grow">
+    <div className="flex items-center">
+      <span className="flex-none font-medium text-2xl text-blue-500">petName</span>
+      <span className="flex-grow text-right">aptDate</span>
+    </div>
+    <div><b className="font-bold text-blue-500">Owner:</b> ownerName</div>
+    <div className="leading-tight">aptNotes</div>
+  </div>
+</li>
+
+  ))
+}
+</ul>
     </div>
   );
 }
