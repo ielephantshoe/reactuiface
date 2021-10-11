b871cba9c3df7bf28e27f7b1c600d587a675597c
butterflydesign <hey@butterflydesign.app>
2021-10-10 22:39:44 -0400

    search algorithm

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
   const filteredAppointments = appointmentList.filter(item =>{
     return (
       item.petName.toLowerCase().includes(query.toLowerCase()) ||
       item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-      item.aptNotes.toLowerCase().includes(query.toLowerCase()) 
-    )
-  })
+      item.aptNotes.toLowerCase().includes(query.toLowerCase()))
+  }).sort((a,b) => {
+      let order = (orderBy === 'asc') ? 1 : -1;
+      return (
+        a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 : 1 * order
+      )
+  });
   const fetchData = useCallback(() => {
     fetch('./data.json')
     .then(response => response.json())
@@ -31,7 +37,11 @@ function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">   
         <h1 className='text-5xl'><BiArchive/> Vetrinary Appointments</h1>
-       <Search query={query} onQueryChange={myQuery => setQuery(myQuery)} />
+       <Search orderBy={orderBy}
+        onOrderBy={orderBy}
+        onOrderByChange={myOrder=>setOrderBy(myOrder)}
+        onSortByChange={mySort=>setSortBy(mySort)}
+        query={query} onQueryChange={myQuery => setQuery(myQuery)} />
        <AddAppointment/>
 <ul>
 {
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import { BiCheck } from "react-icons/bi";
-function Dropdown({toggle}) {
+function Dropdown({toggle, orderBy, onOrderByChange, sortBy, onSortByChange}) {
     if(!toggle) return null
     else return (
         <div className="origin-top-right absolute right-0 mt-2 w-56
       rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
       <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
         <div
+          onClick={()=> onSortByChange('petName')}
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Pet Name <BiCheck /></div>
+          role="menuitem">Pet Name { sortBy === 'petName' && <BiCheck />}</div>
         <div
+
+          onClick={()=> onSortByChange('ownerName')}
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Owner Name  <BiCheck /></div>
+          role="menuitem">Owner Name  { sortBy === 'ownerName' && <BiCheck />}</div>
         <div
+
+          onClick={()=> onSortByChange('aptDate')}
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Date <BiCheck /></div>
+          role="menuitem">Date { sortBy === 'aptDate' && <BiCheck />}</div>
         <div
+
+          onClick={()=> onSortByChange('asc')}
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
-          role="menuitem">Asc <BiCheck /></div>
+          role="menuitem">Asc { sortBy === 'asc' && <BiCheck />}</div>
         <div
+
+          onClick={()=> onSortByChange('desc')}
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Desc <BiCheck /></div>
+          role="menuitem">Desc { sortBy === 'desc' && <BiCheck />}</div>
       </div>
     </div>
 
